3058234da16d62938d9c94db59d3bc93c863eb6d
David Robson <david.robson@bookitude.com>
2013-08-31 12:48:13 +0100

    Added less files and maven plugin

--- Exposure/ExposureWebApp/src/main/java/com/exposure/webapp/base/page/AbstractPage.java
+++ Exposure/ExposureWebApp/src/main/java/com/exposure/webapp/base/page/AbstractPage.java
@@ -1,5 +1,8 @@
 package com.exposure.webapp.base.page;
 
+import org.apache.wicket.markup.head.CssUrlReferenceHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
@@ -10,5 +13,14 @@ import org.apache.wicket.markup.html.WebPage;
 @SuppressWarnings("serial")
 public abstract class AbstractPage extends WebPage
 {
+	@Override
+	public void renderHead(IHeaderResponse response) 
+	{
+	    super.renderHead(response);
 
+	    response.render(JavaScriptHeaderItem.forReference(getApplication().getJavaScriptLibrarySettings()
+	            .getJQueryReference()));  
+	    
+		response.render(CssUrlReferenceHeaderItem.forUrl("css/main.css"));
+	}
 }
